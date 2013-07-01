7f7ab682f3d20c88062760c7c3ef47e528aec6bc
Paul Frandsen <frandsen@eden.rutgers.edu>
2013-06-30 20:22:25 -0600

    added wrapper for determining appropriate number of k's

--- kmeans_wrapper.py
+++ kmeans_wrapper.py
@@ -18,23 +18,55 @@ from sklearn.preprocessing import scale
 from sklearn.cluster import KMeans
 from collections import defaultdict
 
+# To do: make wss work on the multidimensional data
 
-def kmeans_wrapper(dictionary, max_ks = 25):
+def kmeans_wrapper(dictionary, max_ks = 10):
     '''This function performs kmeans on
     a specified number of different k's on a dictionary parsed
     from a *_phyml_lk.txt file. It then calculates
     the within sum of squares (wss) of the k's and returns
     the optimal number of k's and a dictionary of
     the k's and sites belonging to the k's as output
-    determined by the first time the wss shows less than
-    is less than 1/10 with the previous number of k's
+    determined by the first time the wss a decrease that is 1/10
+    of the initial decrease
     '''
-    # Set variables for best num_ks and best dictionary
-    # Set variable for most recent wss and new wss
-    # Perform kmeans starting with a k of 2
-    # When characters are returned, create list of sites and
-    # calculate wss on each cluster
-    pass
+    # Start variable counters
+    # Currently we do kmeans with one k for the first cluster, this
+    # can be modified to start with two k's. This will results in
+    # a slightly larger amount of clusters, we should check the change
+    # in AIC scores of both methods and choose one.
+    count = 1
+    sum_wss = 0
+    new_wss = 0
+    list_of_wss = []
+    # Calculate a bunch of kmeans on however many max_ks are determined
+    for i in range(max_ks):
+        # Run kmeans with each count
+        site_categories = kmeans(dictionary, number_of_ks = count)
+        likelihood_lists = make_likelihood_list(dictionary, site_categories[1])
+        # Set previous wss
+        previous_wss = new_wss
+        # Calculate new wss
+        new_wss = wss(likelihood_lists)
+        list_of_wss.append(new_wss)
+        # Keep the first wss value
+        if count == 1:
+            first_wss = new_wss
+        # Find out the initial decrease
+        if count == 2:
+            initial_decrease = first_wss - new_wss
+            print "The initial decrease is " + str(initial_decrease) + "!"
+        # Find out if the most recent decrease is less than 10% of the
+        # initial decrease
+        # print "New wss: " + str(new_wss)
+        if count > 2:
+            decrease = previous_wss - new_wss
+            # print "decrease: " + str(decrease)
+            if decrease < ((0.1) * initial_decrease):
+                # print "You're finished!"
+                return site_categories
+        count += 1
+    return site_categories
 
 def ftest(likelihood_dict):
     '''This function takes a site likelihood dictionary for different
@@ -78,7 +110,7 @@ def wss(likelihood_lists):
     return within_sum_of_squares
 
 def make_likelihood_list(likelihood_dict, site_categories):
-    '''Takes a scaled_array and site categories
+    '''Takes a likelihood dictionary and site categories
     as input and returns a list of tuples with centroids 
     '''
     new_like_dict = {k: [likelihood_dict[i][0] for i in v] for k, v in site_categories.items()}
@@ -99,7 +131,9 @@ if __name__ == "__main__":
     stop = time.clock()
     print "RAxML took " + str(stop-start) + " seconds!"
     likelihood_dict = raxml_likelihood_parser("RAxML_perSiteLLs." + outfile_command2)
-    site_categories = kmeans(likelihood_dict, number_of_ks = 4)[1]
-    likelihood_lists = make_likelihood_list(likelihood_dict, site_categories)
-    print wss(likelihood_lists)
+    # site_categories = kmeans(likelihood_dict, number_of_ks = 4)[1]
+    # likelihood_lists = make_likelihood_list(likelihood_dict, site_categories)
+    # print likelihood_lists
+    # print wss(likelihood_lists)
+    print kmeans_wrapper(likelihood_dict)
 
